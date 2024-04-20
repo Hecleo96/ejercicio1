0cf2eed60064118b85dfe73dad3f47b6f8c5fea0
HECTOR LEONEL RUIZ TREJO <ISNP645615@ugb.edu.sv>
2024-04-20 10:28:29 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        //Ejercicio que muestre si cada número que se teclea es positivo o negativo
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número (presiona 0 para salir): ");
+
+            while (true)
+            {
+                Console.Write("Ingresa un numero: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    Console.WriteLine("Hasta luego!");
+                    break;
+                }
+                else if (numero > 0)
+                {
+                    Console.WriteLine("El numero es positivo.");
+                }
+                else
+                {
+                    Console.WriteLine("El numero es negativo.");
+                }
+        }
+    }
+}
+}
+
